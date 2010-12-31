f21be5285fed6dcaebce9718e6cd9e3e548f0dd3
Un1matr1x <admin@un1matr1x.de>
2010-12-31 09:24:15 +0100

    pink unicorns over there »»»
    Signed-off-by: Un1matr1x <admin@un1matr1x.de>

--- root/install/index.php
+++ root/install/index.php
@@ -17,8 +17,6 @@ $phpbb_root_path = (defined('PHPBB_ROOT_PATH')) ? PHPBB_ROOT_PATH : '../';
 $phpEx = substr(strrchr(__FILE__, '.'), 1);
 include($phpbb_root_path . 'common.' . $phpEx);
 
-define('IN_PHPBB', true);
-include($phpbb_root_path . 'common.' . $phpEx);
 $user->session_begin();
 $auth->acl($user->data);
 $user->setup(); 
